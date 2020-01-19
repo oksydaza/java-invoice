c933b2db5cf20c4fabd744700a8a2dc842d7f996
Magdalena Wlazlo <magdawlazlo@gmail.com>
2020-01-19 11:11:35 +0100

    commit in ecplipse

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,24 +10,28 @@ public abstract class Product {
 	private final BigDecimal taxPercent;
 
 	protected Product(String name, BigDecimal price, BigDecimal tax) {
+		if (name == null || name.equals("")) {
+			throw new IllegalArgumentException("Product name cannot be null");
+		}
 		this.name = name;
 		this.price = price;
 		this.taxPercent = tax;
-	}
+	} 
 
 	public String getName() {
 		return name;
 	}
 
 	public BigDecimal getPrice() {
-		return null; 
+		return price;  
 	}
 
 	public BigDecimal getTaxPercent() {
 		return taxPercent;
 	}
-
+ 
 	public BigDecimal getPriceWithTax() {
 		return price.add(price.multiply(taxPercent));
-	}
+	} 
 }
+ 
